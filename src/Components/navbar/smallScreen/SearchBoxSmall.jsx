d398889c14f215cb6f2d22f4161faa9f8de6bda5
devwithsmile <devwithsmile007@gmail.com>
2024-06-08 21:09:56 +0530

    trying to fix navbar for small screen -1

--- src/Components/navbar/smallScreen/SearchBoxSmall.jsx
+++ src/Components/navbar/smallScreen/SearchBoxSmall.jsx
@@ -7,7 +7,7 @@ function SearchBoxSmall({handleInputChange}) {
         <div className="relative w-[13%]"> {/* Add margin to the right of the logo */}
             <input type="text"
                 placeholder="Search"
-                className="pl-8 pr-3 py-1 rounded-lg focus:outline-none focus:ring focus:border-blue-300 dark:bg-text_dark dark:text-text_light dark:placeholder-gray-400"
+                className="w-[280%] pl-8 pr-3 py-1 rounded-lg focus:outline-none focus:ring focus:border-blue-300 dark:bg-text_dark dark:text-text_light dark:placeholder-gray-400"
                 onChange={handleInputChange}
             />
             <FontAwesomeIcon icon={faSearch} className="absolute left-2 top-2 text-gray-400 dark:text-gray-300" />
